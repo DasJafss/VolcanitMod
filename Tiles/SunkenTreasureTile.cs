f893120b848786e441e740ca3eb8049296b4e92e
DasJafss <55845277+DasJafss@users.noreply.github.com>
2020-03-26 13:47:34 -0500

    Add Sunken Treasure Tile

---
+++ Tiles/SunkenTreasureTile.cs
@@ -0,0 +1,127 @@
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using static Terraria.ModLoader.ModContent;
+
+namespace Volcanit.Tiles
+{
+	public class SunkenTreasureTile : ModTile
+	{
+		public override void SetDefaults() {
+			Main.tileSpelunker[Type] = true;
+			Main.tileContainer[Type] = true;
+			Main.tileShine2[Type] = true;
+			Main.tileShine[Type] = 1200;
+			Main.tileFrameImportant[Type] = true;
+			Main.tileNoAttach[Type] = true;
+			Main.tileValue[Type] = 500;
+			TileID.Sets.HasOutlines[Type] = true;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+			TileObjectData.newTile.Origin = new Point16(0, 1);
+			TileObjectData.newTile.HookCheck = new PlacementHook(new Func<int, int, int, int, int, int>(Chest.FindEmptyChest), -1, 0, true);
+			TileObjectData.newTile.CoordinateHeights = new int[]{ 16, 16 };
+			TileObjectData.newTile.HookPostPlaceMyPlayer = new PlacementHook(new Func<int, int, int, int, int, int>(Chest.AfterPlacement_Hook), -1, 0, false);
+			TileObjectData.newTile.AnchorInvalidTiles = new[] { 127 };
+			TileObjectData.newTile.StyleHorizontal = true;
+			TileObjectData.newTile.LavaDeath = false;
+			TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile | AnchorType.SolidWithTop | AnchorType.SolidSide, TileObjectData.newTile.Width, 0);
+			TileObjectData.addTile(Type);
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Sunken Treasure");
+			AddMapEntry(new Color(200, 200, 200), name);
+			dustType = 38;
+			disableSmartCursor = true;
+			adjTiles = new int[] { TileID.Containers };
+			chest = "Sunken Treasure";
+			drop = mod.ItemType("SunkenTreasure");
+		}
+
+		public override bool HasSmartInteract() => true;
+
+		public override void KillMultiTile(int i, int j, int frameX, int frameY) {
+			Item.NewItem(i * 16, j * 16, 32, 32, drop);
+			Chest.DestroyChest(i, j);
+		}
+
+		public override bool NewRightClick(int i, int j) {
+			Player player = Main.LocalPlayer;
+			Tile tile = Main.tile[i, j];
+			Main.mouseRightRelease = false;
+			int left = i;
+			int top = j;
+			if (tile.frameX % 36 != 0) {
+				left--;
+			}
+			if (tile.frameY != 0) {
+				top--;
+			}
+			if (player.sign >= 0) {
+				Main.PlaySound(SoundID.MenuClose);
+				player.sign = -1;
+				Main.editSign = false;
+				Main.npcChatText = "";
+			}
+			if (Main.editChest) {
+				Main.PlaySound(SoundID.MenuTick);
+				Main.editChest = false;
+				Main.npcChatText = "";
+			}
+			if (player.editedChestName) {
+				NetMessage.SendData(33, -1, -1, NetworkText.FromLiteral(Main.chest[player.chest].name), player.chest, 1f, 0f, 0f, 0, 0, 0);
+				player.editedChestName = false;
+			}
+				else {
+					int chest = Chest.FindChest(left, top);
+					if (chest >= 0) {
+						Main.stackSplit = 600;
+						if (chest == player.chest) {
+							player.chest = -1;
+							Main.PlaySound(SoundID.MenuClose);
+						}
+						else {
+							player.chest = chest;
+							Main.playerInventory = true;
+							Main.recBigList = false;
+							player.chestX = left;
+							player.chestY = top;
+							Main.PlaySound(player.chest < 0 ? SoundID.MenuOpen : SoundID.MenuTick);
+						}
+						Recipe.FindRecipes();
+					}
+				}
+			return true;
+		}
+
+		public override void MouseOver(int i, int j) {
+			Player player = Main.LocalPlayer;
+			Tile tile = Main.tile[i, j];
+			int left = i;
+			int top = j;
+			if (tile.frameX % 36 != 0) {
+				left--;
+			}
+			if (tile.frameY != 0) {
+				top--;
+			}
+			int chest = Chest.FindChest(left, top);
+			player.showItemIcon2 = -1;
+			if (chest < 0) {
+				player.showItemIconText = Language.GetTextValue("LegacyChestType.0");
+			}
+			else {
+				player.showItemIconText = Main.chest[chest].name.Length > 0 ? Main.chest[chest].name : "Sunken Treasure";
+				if (player.showItemIconText == "Sunken Treasure") {
+					player.showItemIcon2 = ItemType<Items.SunkenTreasure>();
+				}
+			}
+			player.noThrow = 2;
+			player.showItemIcon = true;
+		}
+	}
+}
