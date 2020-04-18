6f2d2927dbeb52a7f93e574eb48e3030d9118acb
DasJafss <55845277+DasJafss@users.noreply.github.com>
2020-04-18 15:26:26 -0500

    Add Calamitous Stuff (Part 1)

---
+++ Items/CalamitousDagger.cs
@@ -0,0 +1,40 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Volcanit.Items
+{
+	public class CalamitousDagger : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+			DisplayName.SetDefault("Calamitous Dagger");
+		}
+
+		public override void SetDefaults() 
+		{
+			item.damage = 300;
+			item.melee = true;
+			item.width = 70;
+			item.height = 88;
+			item.useTime = 13;
+			item.useAnimation = 13;
+			item.crit = 70;
+			item.useStyle = 1;
+			item.knockBack = 17;
+			item.value = 150000;
+			item.rare = 12;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+		}
+
+		public override void AddRecipes() 
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(null, "CalamitousBar", 23);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
---
+++ Items/CalamitousScythe.cs
@@ -0,0 +1,44 @@
+using Volcanit.Projectiles;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Volcanit.Items
+{
+	public class CalamitousScythe : ModItem
+	{
+		public override void SetDefaults() {
+			item.damage = 350;
+			item.useStyle = 5;
+			item.useAnimation = 18;
+			item.useTime = 24;
+			item.shootSpeed = 7.5f;
+			item.knockBack = 6.5f;
+			item.width = 32;
+			item.height = 32;
+			item.scale = 1f;
+			item.rare = 11;
+			item.value = 150000;
+
+			item.melee = true;
+			item.noMelee = true;
+			item.noUseGraphic = true;
+			item.autoReuse = true;
+			item.UseSound = SoundID.Item1;
+			item.shoot = ProjectileType<CalamitousScytheProjectile>();
+		}
+
+		public override bool CanUseItem(Player player) {
+			return player.ownedProjectileCounts[item.shoot] < 1;
+		}
+		public override void AddRecipes() 
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(null, "CalamitousBar", 28);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
